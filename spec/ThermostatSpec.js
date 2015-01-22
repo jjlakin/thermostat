cd205ffb5cabc003cd9793121d334ba0ecdb75b2
jjlakin <j.lakin@gmail.com>
2015-01-22 13:30:12 +0000

    added html page

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -20,6 +20,53 @@ describe('Thermostat', function() {
 		it('that decreases with down input', function() {
 			thermostat.adjustTemperature('down')		
 			expect(thermostat.temperature).toBe(19);
+		});	
+
+		it('minimum of 10', function() {
+			thermostat.minimum()
+			expect(thermostat.temperature).toBe(10);
+		});
+
+		it('minimum of 10 that it cannot go bellow', function() {
+			thermostat.minimum();
+			thermostat.adjustTemperature('down');	
+			expect(thermostat.temperature).toBe(10);
+		});
+
+	});
+
+	describe('cannot', function() {
+
+		it('go above 25 when energy saver is on', function() {
+			thermostat.maximum();
+			thermostat.adjustTemperature('up');	
+			expect(thermostat.temperature).toBe(25);
+		});
+
+		it('go above 32 when energy saver is off ', function() {
+			thermostat.energySaver('off');
+			thermostat.maximum();
+			thermostat.adjustTemperature('up');	
+			expect(thermostat.temperature).toBe(32);
+		});
+
+	});
+
+	describe('recognises when it is', function() {
+
+		it('saving energy', function() {
+			thermostat.minimum();
+			expect(thermostat.energyStatus()).toBe('saving energy');
+		});
+
+		it('using an ordinary amount of energy', function() {
+			expect(thermostat.energyStatus()).toBe('normal energy');
+		});
+
+		it('burning fuel ', function() {
+			thermostat.energySaver('off');
+			thermostat.maximum();
+			expect(thermostat.energyStatus()).toBe('burning energy');
 		});
 
 	});
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,14 +1,38 @@
 var Thermostat = function () {
 
 	this.temperature = 20;
+	this.maxTemp = 25;
 
 };
 
 	
 
 Thermostat.prototype.adjustTemperature = function(change) {
-	 if (change === 'up') this.temperature += 1 ;
-	 if (change === 'down') this.temperature -= 1 ;
+	
+	if (change === 'up') 
+		if (this.temperature < this.maxTemp)this.temperature += 1 ;
+	if (change === 'down') 
+		if (this.temperature > 10) this.temperature -= 1 ;
+};
+
+Thermostat.prototype.minimum = function() {
+	this.temperature = 10
+};
+
+Thermostat.prototype.maximum = function() {
+	this.temperature = this.maxTemp
+};
+
+Thermostat.prototype.energySaver = function(status) {
+	if (status === 'on')this.maxTemp = 25;
+	if (status === 'off')this.maxTemp = 32;
+};
+
+Thermostat.prototype.energyStatus = function() {
+	if (this.temperature < 18) return 'saving energy';
+	if (this.temperature < 25) return 'normal energy';
+	if (this.temperature >= 25) return 'burning energy';
+
 };
 
 
